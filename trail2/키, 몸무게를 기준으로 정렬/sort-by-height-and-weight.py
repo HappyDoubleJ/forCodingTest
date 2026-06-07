4104c2677e5a9aad5fc460c94ae07456439abe01
허준재 <90675043+HappyDoubleJ@users.noreply.github.com>
2026-06-07 13:50:39 +0900

    [Passed] 키, 몸무게를 기준으로 정렬 (50ms, 16MB)

---
+++ trail2/키, 몸무게를 기준으로 정렬/sort-by-height-and-weight.py
@@ -0,0 +1,26 @@
+n = int(input())
+name = []
+height = []
+weight = []
+for _ in range(n):
+    n_i, h_i, w_i = input().split()
+    name.append(n_i)
+    height.append(int(h_i))
+    weight.append(int(w_i))
+
+class man:
+    def __init__(self, name, height, weight):
+        self.name = name
+        self.height = height
+        self.weight = weight
+
+mans = []
+
+for i in range(n):
+    t = man(name[i], height[i], weight[i])
+    mans.append(t)
+
+mans.sort(key = lambda x : (x.height, -x.weight))
+
+for i in range(n):
+    print(mans[i].name, mans[i].height, mans[i].weight)
