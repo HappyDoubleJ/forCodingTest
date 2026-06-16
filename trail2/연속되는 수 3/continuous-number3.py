79fe9260eaad368e3e3fabb633dd4c73791b8fe1
허준재 <90675043+HappyDoubleJ@users.noreply.github.com>
2026-06-16 15:02:54 +0900

    [Passed] 연속되는 수 3 (60ms, 16MB)

---
+++ trail2/연속되는 수 3/continuous-number3.py
@@ -0,0 +1,16 @@
+N = int(input())
+arr = [int(input()) for _ in range(N)]
+
+
+max = 0
+cnt = 0
+
+for i in range(N):
+    if i == 0 or arr[i] * arr[i - 1] >= 0:
+        cnt += 1 
+    else:
+        cnt = 1
+    if cnt >= max:
+        max = cnt
+
+print(max)
