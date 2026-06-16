11826a5136f60ae1b316f34bde4a4f537f41e07a
허준재 <90675043+HappyDoubleJ@users.noreply.github.com>
2026-06-16 15:06:00 +0900

    [Passed] 연속되는 수 4 (53ms, 16MB)

---
+++ trail2/연속되는 수 4/continuous-number4.py
@@ -0,0 +1,15 @@
+n = int(input())
+arr = [int(input()) for _ in range(n)]
+
+max = 0
+cnt = 0
+
+for i in range(n):
+    if n == 0 or arr[i] > arr[i - 1]:
+        cnt += 1
+    else:
+        cnt = 1
+    if cnt >= max:
+        max = cnt
+
+print(max)
