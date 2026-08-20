dd8ecd924a7ad99a0602bfdda53e6ac0e25680d0
허준재 <90675043+HappyDoubleJ@users.noreply.github.com>
2026-08-20 19:38:30 +0900

    [Passed] 특정 조건에 맞게 K개 중에 1개를 N번 뽑기 (293ms, 27MB)

--- trail4/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
+++ trail4/특정 조건에 맞게 K개 중에 1개를 N번 뽑기/n-permutations-of-k-with-repetition-under-constraint.py
@@ -2,33 +2,20 @@ K, N = map(int, input().split())
 
 result = []
 
-def sy(n, k):
-    if n == 0:
+def sy():
+
+    if len(result) == N:
         print(*result)
         return
+    
 
-    for i in range(1, k + 1):
+    for i in range(1, K + 1):
+        if len(result) >= 2:
+            if result and result[-1] == result[-2] == i:
+                continue
+        
+        result.append(i)
+        sy()
+        result.pop()
 
-        if result and i == result[-1]:
-            result.append(i)
-
-            # i 하나를 넣었더니 길이가 완성된 경우
-            if n == 1:
-                sy(n - 1, k)
-
-            else:
-                # 다음에는 i와 다른 숫자를 강제로 넣기
-                for j in range(1, k + 1):
-                    if j != i:
-                        result.append(j)
-                        sy(n - 2, k)
-                        result.pop()
-
-            result.pop()
-
-        else:
-            result.append(i)
-            sy(n - 1, k)
-            result.pop()
-
-sy(N, K)
+sy()
